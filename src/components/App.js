57d0c8b94250f643144552466f6635c3b75a348f
jetsoanalin <jetyjetso@gmail.com>
2020-07-28 02:38:59 +0530

    EthersJS: Blockchain Connection Added with Functions

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,11 @@ import web3 from 'web3';
 import evidence from '../abis/evidence.json';
 // Added this new Library ipfs-api
 const ipfsAPI = require('ipfs-api');
+const ethers = require('ethers')
+
+
+
+
 // New code according to ipfs-api
 const ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
 var memeHash="";
@@ -15,6 +20,48 @@ var unique_id=0;//store that unique id here
 memeHashes[0]=unique_id;
 var pointer_memeHashes=1;//this pointer is for memeHashes array
 var uid = '';
+
+
+// Blockchain Connection
+// Configure blockchain
+// Address : 0xc599CC35B2207e2e6E6Ca02845E39dbBbFB05417
+// PK :  97145712ceda47b7886ba94a55f76038bb95ddb41f43fbb42189866878f453ae
+// ContractTX : https://rinkeby.etherscan.io/tx/0xf9224a6abfeec04122f99e20ec300c68c126cccdf2a0a14ac9767ff0d9059e15
+const pk = "0x97145712ceda47b7886ba94a55f76038bb95ddb41f43fbb42189866878f453ae"
+const provider = new ethers.getDefaultProvider("rinkeby")
+const wallet = new ethers.Wallet(pk,provider) 
+
+const aharyaContractAdd = "0x7560cf40c71ed503b7e3dd98cc753261b09f61bd"
+const aharyaContractAbi = [ { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "ipfsDatabase", "outputs": [ { "internalType": "uint256", "name": "id", "type": "uint256" }, { "internalType": "string", "name": "name", "type": "string" }, { "internalType": "string", "name": "hash", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "", "type": "string" } ], "name": "ipfsID", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "_evidenceHash", "type": "string" }, { "internalType": "string", "name": "_name", "type": "string" } ], "name": "set", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "nonpayable", "type": "function" } ]
+
+
+// Loading Main Contract
+const aharyaContract = new ethers.Contract(aharyaContractAdd, aharyaContractAbi, wallet)
+console.log(aharyaContract)
+
+// Contract Functionality
+async function addToBlockchain(uid,memehash) { 
+  const databaseFetch = await aharyaContract.functions.set(memehash,uid);
+  const from = databaseFetch.from
+  const to = databaseFetch.to
+  const hash = databaseFetch.hash
+  console.log("FromAddress: "+from+" ToAddress: "+to+" rinkebyTXhash: "+hash) 
+};
+
+async function viewFromBlockchain(uid) { 
+  console.log("reacing here")
+  const databasePosFetch = await aharyaContract.functions.ipfsID(uid);
+  const floatValue = ethers.utils.formatUnits(databasePosFetch,'wei');
+  const EvidencePos = parseInt(floatValue);
+  console.log(EvidencePos) 
+  const databaseFetch = await aharyaContract.functions.ipfsDatabase(EvidencePos);
+  const id = databaseFetch.id;
+  const name = databaseFetch.name;
+  const hash = databaseFetch.hash;
+  console.log("ID: "+id+" Name/UID: "+name+" Hash: "+hash)
+
+};
+
 class App extends Component {
 
   constructor(props){
@@ -25,10 +72,13 @@ class App extends Component {
       contract:null,
     };
   }
+  
   buttonEnterIdListner = (event) => {
     event.preventDefault()
     uid = document.getElementById('uname').value;
-    alert(uid)
+    // alert(uid)
+    // Send UID and Hash
+    addToBlockchain(uid,memeHash);
     // Alert.alert(uname);
   }
   /*async loadWeb3(){
@@ -41,6 +91,8 @@ class App extends Component {
       window.alert('please use wallet');
     }
   }*/
+
+
   
   captureFile = (event)=>{
     event.preventDefault()
@@ -67,10 +119,14 @@ class App extends Component {
         memeHash=memeHash.concat(temp)
         console.log("click on submit")
       })
+      
     }
+
   }
+  
   /*on submit code*/
   onSubmit=(event)=>{
+    
     console.log("click on done when all hashes are uploaded")
     console.log(memeHash)
     event.preventDefault()
@@ -85,9 +141,9 @@ class App extends Component {
   }
   onDone=(event)=>{
     event.preventDefault();
-      //here you need to call done method and send the memeHashes array
-      console.log("final hash is")
-      console.log(memeHash) 
+      //here you have to give UID of which data we want to fetch
+      uid = document.getElementById('uname').value;
+      viewFromBlockchain(uid)
   }
   render() {
     return (
@@ -119,9 +175,9 @@ class App extends Component {
                     {/* <input type="text" id="fname" name="fname"/> */}
                     {/* <input type="submit" id="uname" value="Enter your unique id" onChange={this.onUsername}/>  */}
                     <input placeholder="Enter your unique id" id="uname"/> 
-                    <button onClick={this.buttonEnterIdListner} >Submit uid</button>
+                    <button onClick={this.buttonEnterIdListner} >Add to Blockchain</button>
                     <div className="submit">
-                      <button type="submit" id="done" value="Submit" onChange={this.onDone}>Done</button>
+                      <button type="submit" id="done" value="Submit" onClick={this.onDone}>ViewData</button>
                     </div>
                   </form>
                 </div>
